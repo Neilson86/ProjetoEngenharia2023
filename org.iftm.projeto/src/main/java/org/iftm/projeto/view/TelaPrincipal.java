dc1791d9acc16bdd2b33e4f4347480dde57e04ae
neils <neils@FamiliaLima>
2023-06-05 20:29:21 -0300

    Versão 1.

--- org.iftm.projeto/src/main/java/org/iftm/projeto/view/TelaPrincipal.java
+++ org.iftm.projeto/src/main/java/org/iftm/projeto/view/TelaPrincipal.java
@@ -3,10 +3,17 @@ package org.iftm.projeto.view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JInternalFrame;
+import javax.swing.JToolBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class TelaPrincipal {
 
-	private JFrame frame;
+	private JFrame frmSchoolRobots;
 
 	/**
 	 * Launch the application.
@@ -16,7 +23,7 @@ public class TelaPrincipal {
 			public void run() {
 				try {
 					TelaPrincipal window = new TelaPrincipal();
-					window.frame.setVisible(true);
+					window.frmSchoolRobots.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +42,49 @@ public class TelaPrincipal {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmSchoolRobots = new JFrame();
+		frmSchoolRobots.setTitle("SCHOOL ROBOTS");
+		frmSchoolRobots.setBounds(100, 100, 394, 492);
+		frmSchoolRobots.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar barramenu = new JMenuBar();
+		frmSchoolRobots.setJMenuBar(barramenu);
+		
+		JMenu COORDENADOR = new JMenu("COORDENADOR");
+		barramenu.add(COORDENADOR);
+		
+		JMenuItem GERIR_PROFESSORES = new JMenuItem("GERIR PROFESSOR");
+		COORDENADOR.add(GERIR_PROFESSORES);
+		
+		JMenuItem CADASTRAR_DISCIPLINAS = new JMenuItem("CADASTAR DISCIPLINA");
+		COORDENADOR.add(CADASTRAR_DISCIPLINAS);
+		
+		JMenuItem GERIR_DADOS_ALUNOS = new JMenuItem("GERIR DADOS ALUNO");
+		COORDENADOR.add(GERIR_DADOS_ALUNOS);
+		
+		JMenu ATENDENTE = new JMenu("ATENDENTE");
+		barramenu.add(ATENDENTE);
+		
+		JMenuItem CADASTRAR_CURSO = new JMenuItem("CADASTRAR CURSO");
+		ATENDENTE.add(CADASTRAR_CURSO);
+		
+		JMenuItem CADASTRAR_PROFESSOR = new JMenuItem("CADASTRAR PROFESSOR");
+		ATENDENTE.add(CADASTRAR_PROFESSOR);
+		
+		JMenuItem CADASTRAR_ALUNO = new JMenuItem("CADASTRAR ALUNO");
+		ATENDENTE.add(CADASTRAR_ALUNO);
+		
+		JMenu ALUNO = new JMenu("ALUNO");
+		barramenu.add(ALUNO);
+		
+		JMenuItem CONSULTAR_DISCIPLINA = new JMenuItem("CONSULTAR DISCIPLINA");
+		ALUNO.add(CONSULTAR_DISCIPLINA);
+		
+		JMenuItem CONSULTAR_CURSO = new JMenuItem("CONSULTAR CURSO");
+		ALUNO.add(CONSULTAR_CURSO);
+		
+		JMenuItem REALIZAR_MATRICULA = new JMenuItem("REALIZAR MATRICULA");
+		ALUNO.add(REALIZAR_MATRICULA);
 	}
 
 }
